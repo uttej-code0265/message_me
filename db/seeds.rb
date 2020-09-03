7eee6cc9fb1d5b9669b05c11cedd86aaa0ab12a4
Uttej Kumar <uttejkumarponugoti@gmail.com>
2020-09-03 15:52:30 +0530

    messages from backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username:"uttej",password:"password")   
+User.create(username:"manoj",password:"password")   
+User.create(username:"bannu",password:"password")   
+User.create(username:"dady",password:"password")   
+User.create(username:"mummy",password:"password")   
