16098b0b59a1fa7f4ed365ed5aa18f54a2bfe73d
Uttej Kumar <uttejkumarponugoti@gmail.com>
2020-09-04 15:32:32 +0530

    authentication system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,22 @@ def new
 
 end
 
+def create
+    user=User.find_by(username: params[:session][:username])
+    if user  && user.authenticate(params[:session][:password])
+    session[:user_id]=user.id
+    flash[:success]="You have succesfully logged in"
+    redirect_to root_path
+    else
+    flash.now[:error]="There is a problem in logging in"
+    render 'new'
+    end
+end
+
+def destroy
+session[:user_id]=nil
+flash[:success]="You have succesfully logged out"
+redirect_to login_path
+end
 
 end
